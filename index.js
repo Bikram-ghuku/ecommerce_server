15a495bda09fdd79c5ebfb7e1a628714bd7f7095
Bikram-ghuku <bikramghuku05@gmail.com>
2023-07-31 20:51:36 +0530

    added database storing and query

--- index.js
+++ index.js
@@ -9,18 +9,39 @@ server.use(bodyParser.json())
 mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => console.log('DataBase Connected!'));
 
-server.get('/', (req, res)=>{
-    res.send("noob")
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema)
+
+
+server.post('/register', async (req, res)=>{
+    const doc = await User.find({email: req.body.email})
+    if(doc==[]){
+        let user = new User();
+        user.name = req.body.name
+        user.email = req.body.email
+        user.password = req.body.pswd
+
+        res.send({code: 'ok'})
+    }
+    else{
+        res.send({code: 'Account Already Present! Use a different account'})
+    }
 })
 
-server.post('/register', (req, res)=>{
-    console.log(req.body)
-})
-
-server.post('/login', (req, res)=>{
-    var mailAdd = req.body.email
-    res.send({name:'Hello world', email: mailAdd})
-    console.log('data requested')
+server.post('/login', async (req, res)=>{
+    const doc = await User.find({email: req.body.email, password: req.body.pswd})
+    var resData = doc[0]
+    if(resData){
+        res.send({name:resData.name, email:resData.email, code:'ok'})
+    }
+    else{
+        res.send({code:'incorrect username or password'})
+    }
 })
 
 server.listen(8080, ()=>{
