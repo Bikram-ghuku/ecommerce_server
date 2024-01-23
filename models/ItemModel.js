ff5636de19953a8fe6dca9e84fde4afc2129b32d
Bikram Ghuku <bikramghuku05@gmail.com>
2024-01-23 19:53:57 +0530

    refactor: made models folder and added ItemsModel

---
+++ models/ItemModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('./services/Mongodb');
+
+const itemsSchema = new mongoose.Schema({
+    pdtName: String,
+    img: String,
+    desc: String,
+    cost: Number,
+    opts: Array,
+    dispType: String,
+    seller: String,
+    rating: Number
+})
+
+const Items = mongoose.model('items', itemsSchema);
+
+module.exports = Items;
