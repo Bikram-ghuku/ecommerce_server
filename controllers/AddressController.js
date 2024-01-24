0ce8b5d23a8c21091e62dc87dcac58a424647106
Bikram Ghuku <bikramghuku05@gmail.com>
2024-01-24 08:38:06 +0530

    refactor: added addressController to controllers

---
+++ controllers/AddressController.js
@@ -0,0 +1,28 @@
+const Address = require('../models/AddressModel');
+
+const addAddress = async (req, res)=>{
+    let address = new Address();
+    address.uid = req.body.uid
+    address.address = req.body.address
+    address.pin = req.body.pincode
+    address.city = req.body.city
+    address.state = req.body.state
+    address.country = req.body.country
+    address.phone = req.body.phone
+    res.send({code: 'ok'})
+    const user = await User.find({_id: req.body.uid})
+    var addressAr = user[0].address
+    addressAr.push(address._id)
+    address.save()
+    await User.updateOne({_id : req.body.uid}, {address: addressAr})
+}
+
+const getAddress = async (req, res)=>{
+    const doc = await Address.find({uid: req.body.uid})
+    res.send(doc)
+}
+
+module.exports = {
+    addAddress,
+    getAddress
+}
